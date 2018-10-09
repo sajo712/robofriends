2ddaa05dbe229f2818b898e886de886f4e575bc5
sajo712 <sajo712@gmail.com>
2018-10-09 12:07:35 +0200

    Working Robofriends App

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw5 shadows-5'>
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw5 shadow-5'>
       <img alt='robots' src={`http://robohash.org/${id}?200x200`} />
       <div>
         <h2>{name}</h2>
